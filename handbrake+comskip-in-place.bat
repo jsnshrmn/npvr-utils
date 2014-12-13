daf596e2d53dbb2a64f71e7224b9db9210ce6609
Jason Sherman <jsnshrmn@users.noreply.github.com>
2014-12-13 12:43:38 -0600

    made some style changes and fixed typos

--- handbrake+comskip-in-place.bat
+++ handbrake+comskip-in-place.bat
@@ -1,20 +1,23 @@
-@echo off
-setlocal enabledelayedexpansion
-set "file-ext=%1"
-set "startdir=%2"
-set "comskip=start /B /WAIT /D "%HOMEDRIVE%\Program Files (x86)\NPVR\" comskip.exe"
-set "hb-ex=start /B /WAIT /D "%HOMEDRIVE%\Program Files\Handbrake" HandBrakeCLI.exe"
-set "hb-args=--large-file --optimize -e x264 -q 22 --x264-tune fastdecode --maxWidth 1280 --maxHeight 720 --cfr -B 128"
+@ECHO off
+SETLOCAL enableDELayedexpansion
+SET "file-ext=%1"
+SET "startdir=%2"
+SET "comskip=start /B /WAIT /D "%HOMEDRIVE%\Program Files (x86)\NPVR\" comskip.exe"
+SET "hb-ex=start /B /WAIT /D "%HOMEDRIVE%\Program Files\Handbrake" HandBrakeCLI.exe"
+SET "hb-args=--large-file --optimize -e x264 -q 22 --x264-tune fastdecode --maxWidth 1280 --maxHeight 720 --cfr -B 128"
 
-for /r %startdir% %%i in (*.%file-ext%) do (
-	set "inputtemp=%%i"
-	set "subname=!inputtemp:~0,-3!"
-	set "input="%%i""
-	set "output="!subname!m4v""
-	set "transcode=%hb-ex% -i !input! -o !output! %hb-args% > NUL 2>&1"
+FOR /r %startdir% %%i IN (*.%file-ext%) DO (
+	SET "inputtemp=%%i"
+	SET "subname=!inputtemp:~0,-3!"
+	SET "input="%%i""
+	SET "output="!subname!m4v""
+	SET "transcode=%hb-ex% -i !input! -o !output! %hb-args% > NUL 2>&1"
 	!transcode!
-	if errorlevel 1 do exit
+	IF ERRORLEVEL 1 EXIT
 	%comskip% !output!
-	rem del /F !input!
+	rem IF ERRORLEVEL 1 EXIT	
+	rem DEL !input!
+	rem IF ERRORLEVEL 1 EXIT	
+	rem DEL "!subname!nfo"
 )
-endlocal
+ENDLOCAL
